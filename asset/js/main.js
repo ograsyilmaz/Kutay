50c75cda9dd8656e6d37537cf52ffbac552d1839
yilmazogras <yilmazogras@gmail.com>
2017-05-26 17:21:26 +0300

    js

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,85 +1,81 @@
 
-    // ücgen konum belirleme
-    // büyük eşit falan  yap deger kontürlu
+// ücgen konum belirleme
+// büyük eşit falan  yap deger kontürlu
 
-      $( document ).ready(function() {
-                var boyut=$(window).innerHeight();
-                var ucgen=$('.ucgen').innerHeight();
-            
-                var mainblok=$('.main-blok').innerHeight()+$('.ucgen').innerHeight();
-                  var fark=boyut-mainblok;
-                var bottomdeger="";
-                var deger="";
-                  console.log(boyut+ "-"+mainblok );
-                if(fark>0)
-                {
-                  bottomdeger=20;
-           deger= boyut-(mainblok+bottomdeger);
-             console.log(deger + "deger");
-                 
-                       console.log("fark sıfırdan büyük");
-                }
+$(document).ready(function () {
+  var boyut = $(window).innerHeight();
+  var ucgen = $('.ucgen').innerHeight();
 
-                else if (fark===0) {
-                  bottomdeger=0;
-deger= boyut-(mainblok+bottomdeger);
+  var mainblok = $('.main-blok').innerHeight() + $('.ucgen').innerHeight();
+  var fark = boyut - mainblok;
+  var bottomdeger = "";
+  var deger = "";
+  console.log(boyut + "-" + mainblok);
+  if (fark > 0) {
+    bottomdeger = 20;
+    deger = boyut - (mainblok + bottomdeger);
+    console.log(deger + "deger");
 
-                 
-                  console.log("fark sıfıra eşit");
-                   bottomdeger=2;
-                }
-                else {
-                  deger=0;
-                  console.log("fark - deger");
-                  bottomdeger=0;
-                }
+    console.log("fark sıfırdan büyük");
+  }
 
-                  $(".ucgen").css({"margin-top":deger+"px","margin-bottom":bottomdeger+"px"});
+  else if (fark === 0) {
+    bottomdeger = 0;
+    deger = boyut - (mainblok + bottomdeger);
+
+
+    console.log("fark sıfıra eşit");
+    bottomdeger = 2;
+  }
+  else {
+    deger = 0;
+    console.log("fark - deger");
+    bottomdeger = 0;
+  }
+
+  $(".ucgen").css({ "margin-top": deger + "px", "margin-bottom": bottomdeger + "px" });
 });
 
 
 // hamburgen menu açılması fonksiyonu
 
-$('#toggle').click(function() {
-    $(this).toggleClass('active');
-    $('#overlay').toggleClass('open');
+$('#toggle').click(function () {
+  $(this).toggleClass('active');
+  $('#overlay').toggleClass('open');
 });
 
 
 // Porfolio  seçmek için yazılan kod
 
-$(function() {
-		var selectedClass = "";
-		$(".portfolio-list>li").click(function(){ 
-    
-		selectedClass = $(this).attr('id');
-     $(".portolio-item-list").fadeTo(100, 0.1);
-		$(".portolio-item-list div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn().addClass('scale-anm');
+$(function () {
+  var selectedClass = "";
+  $(".portfolio-list>li").click(function () {
+
+    selectedClass = $(this).attr('id');
+    $(".portolio-item-list").fadeTo(100, 0.1);
+    $(".portolio-item-list div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function () {
+      $("." + selectedClass).fadeIn().addClass('scale-anm');
       $(".portolio-item-list").fadeTo(300, 1);
-    }, 300); 
-		
-	});
+    }, 300);
+
+  });
 });
 
 
 
-
-
-
-// $('.ucgen').click(function(){
+$(function () {
+  $(".ucgen").click(function () {
+    var top = $(".portfolio").offset();
   
-//   scrollkonum(#protfolio);
-// });
-
-// /////scroll ücgren
 
 
-// $scrollkonu=function(){
-// var top= $('#'+deger+'').offset();
-// window.screenTop(0,top)
-// }
+    $("body,html").animate({
+      scrollTop: top.top
+    });
 
 
+
+  });
+});
 
